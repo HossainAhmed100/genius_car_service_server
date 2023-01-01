c269e0cac6c9aa79f4f00000e8479692a216c004
HossainAhmed100 <successfulllife101@gmail.com>
2023-01-01 23:42:58 +0600

    mbr

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
+ 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nxph8s3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
