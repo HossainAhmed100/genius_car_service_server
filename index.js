966ae239baba2680e98d046c2be550d3ca77b7c9
HossainAhmed100 <successfulllife101@gmail.com>
2023-01-02 04:40:00 +0600

    corsp

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
-const cors = require("cors");
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT;
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
  
 
