93b1b76cd7db00212b1fa014f4baa3deb355463d
HossainAhmed100 <successfulllife101@gmail.com>
2023-01-02 00:20:51 +0600

    corspolicy

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT;
-
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
  
 
